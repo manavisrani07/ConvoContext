2f77526b65d668099026246d685aaa5edab3910e
Manav Israni <87480105+manavtech07@users.noreply.github.com>
2023-10-29 21:47:03 +0530

    code to run the model on question and content.

---
+++ main.py
@@ -0,0 +1,34 @@
+from transformers import BertTokenizer, BertForQuestionAnswering, pipeline
+
+class QAModel:
+    def __init__(self, model, tokenizer):
+        self.tokenizer = tokenizer
+        self.model = model
+        self.qa_pipeline = pipeline('question-answering', model=self.model, tokenizer=self.tokenizer)
+
+    def answer_question(self, question, context):
+        answer = self.qa_pipeline({
+            'question': question,
+            'context': context
+        })
+        return answer
+
+class ContentExtractor:
+    @staticmethod
+    def extract_context(content, start_index, end_index):
+        # Move the start index backward until a period (".") is found or the start of the content
+        while start_index > 0 and content[start_index] != '.':
+            start_index -= 1
+
+        # Extend the end index until a period (".") is found or the end of the content
+        while end_index < len(content) and content[end_index] != '.':
+            end_index += 1
+
+        # Extract the identified content
+        extracted_content = content[start_index :end_index]  # +1 to exclude the period
+
+        # Split the content into multiple lines
+        max_line_length = 80  # Adjust the line length as needed
+        lines = [extracted_content[i:i + max_line_length] for i in range(0, len(extracted_content), max_line_length)]
+
+        return "\n".join(lines)
